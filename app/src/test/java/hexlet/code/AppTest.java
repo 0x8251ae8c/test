71fbd1e931fb36854f951c2d43628806f69044c6
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-10 22:21:15 +0700

    Fix lint error

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -6,8 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    public void testApp()
-    {
+    public void testApp() {
         assertEquals(true, true);
     }
 }
