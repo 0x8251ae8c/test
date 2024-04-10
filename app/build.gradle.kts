fc41e73fcfd3c832c3b98c55301a783fdeb7953c
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-10 22:00:18 +0700

    Add project files

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    application
+    id("java")
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
