6ec4b0cd167bf14185b455813b31d041b96f5998
0x8251ae8c <99046156+0x8251ae8c@users.noreply.github.com>
2024-04-10 22:15:47 +0700

    Fix test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
     @Test
     public void testApp() {
-        assertEquals(true, false);
+        assertEquals(true, true);
     }
 }
